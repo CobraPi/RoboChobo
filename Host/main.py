07948363c950b5ba3e57a141e487fea0cb45fcf5
Joseph Orduna <jorduna@allendatagraph.com>
2021-12-01 19:25:57 -0500

    echo working, still having timing issues

--- Host/main.py
+++ Host/main.py
@@ -12,42 +12,54 @@ Packet Structure: [start_bit,  // BEGIN
 """
 
     
-SERIAL_PORT = "/dev/cu.usbmodem2142201"
+SERIAL_PORT = "/dev/cu.usbmodem1301"
 BAUD_RATE = 9600
 
+############## Protocol Codes ############
 ACK   = b'\x06'
 END   = b'\x00'
-BEGIN = b'\x01'
+START = b'\x01'
 DEBUG = b'\x08'
 
+############## Control Codes #############
 SENDMOTOR_DATA   = b'\x32'
 REQUEST_IMU_DATA = b'\x33'
+REQUEST_RANGE_DATA = b'\x34'
 
+############## State Codes ################
+STATE_IR_SENSOR_TIMEOUT = b'\x40'
 
 
 def startConnection(com):
     print("Connecting to device...")
     time.sleep(2) 
     com.write(ACK) # send handshake byte to arduino
-    while(com.read() != ACK):
+    while(com.read() != ACK): # wait for arduino to acknowledge
         print(".", end="")
     print("Serial Handshake Successful")
 
 def sendPacket(ser, control_code, data_length, data):
-    ser.write(BEGIN)
+    ser.write(START)
     ser.write(control_code)
     ser.write(data_length)
     if data_length > 0: 
         for i in data:
             ser.write(i)
-        ser.write(END)
+    ser.write(END)
     
 
 if __name__ == "__main__":
     com = serial.Serial(port=SERIAL_PORT, baudrate=BAUD_RATE, bytesize=EIGHTBITS)
     startConnection(com) 
-    com.write(DEBUG)
-    
-    while(True):
-        sendPacket(com, DEBUG, 3, [1,2,3])
-        print(com.read_until(END))
+    #com.write(DEBUG)
+    sendPacket(com, REQUEST_RANGE_DATA, 4, [1,2,3,4])
+    #while(True):
+        #com.write(START)
+        #sendPacket(com, REQUEST_RANGE_DATA, 4, [1,2,3,4])
+        #com.write(SENDMOTOR_DATA)
+        #if(com.read() == START):
+    time.sleep(1)
+    for i in range(com.in_waiting):
+        print(com.read())        
+         #   for i in range(com.in_waiting):
+          #      print(com.read())
