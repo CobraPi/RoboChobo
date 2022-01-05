7eac184cd6c06e41bcb21ec81b01a8a2e5870e56
Joseph Orduna <jorduna@allendatagraph.com>
2022-01-05 01:28:40 -0500

    motor function working

--- Firmware/include/defines.h
+++ Firmware/include/defines.h
@@ -1,10 +1,21 @@
 #define DEBUG 1
 
-#define SERIAL 
+#define BAUD_RATE 9600
 
-#define X_STEP_PIN 2
-#define X_DIR_PIN  5
+#define L_STEP_PIN 2
+#define L_DIR_PIN  5
 
-#define Y_STEP_PIN 4
-#define Y_DIR_PIN  7
+#define R_STEP_PIN 4
+#define R_DIR_PIN  7
+/*
+   Used for pulse generation
+   1/1 step: 62
+   1/2 step: 31
+   1/4 step: 16
+*/
+#define MIN_TICKS 16
 
+#define MAX_ANGLE  40
+#define CG 0
+#define MAX_SPEED 1000.0f
+#define MIN_SPEED 1
--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -18,51 +18,163 @@ Driver ---> Host
 #include <SparkFunLSM9DS1.h>
 #include <defines.h>
 
-#define BAUD_RATE 9600
-
-#define ACK   0x06
-#define END   0x00 
-#define START 0x01
-#define DEBUG 0x08   
-
-#define MOTOR_DATA 0x32
-#define IMU_DATA   0x33
-#define RANGE_DATA 0x34
-
-#define STATE_IR_SENSOR_TIMEOUT 0x40
-
-#define IR_SENSOR_TIMEOUT 500
-
-#define DEBUG 
+void set_pulse_duration(float pulseTime);
+float pulse_time(float speed);
+void start_timer();
+void stop_timer();
 
 bool direction = 1;
 bool moving = 1;
 bool targetChange = 0;
 
+uint16_t skipRightPulse = 0;
+uint16_t skipLeftPulse = 0;
+uint16_t numInterrupts = 0;
 
+float currentAngle = 0;
+float targetAngle = 0.0f;
+
+int32_t actualPosition = 0;
+int32_t targetPosition = 0;
+
+int timer = 0;
+
+float speed = 3;
 
 void setup() {
-    // Sets the two pins as Outputs
-    pinMode(X_STEP_PIN, OUTPUT);
-    pinMode(X_DIR_PIN, OUTPUT);
-    pinMode(Y_STEP_PIN, OUTPUT); 
-    pinMode(Y_DIR_PIN, OUTPUT);
+    TCCR1A = 0;
+    TCCR1B = 0;
+    OCR1A = 0x00FF;                                            // When timer is equal to this value, interrupt is triggered
+    TIMSK1 |= (1 << OCIE1A);                                   // Enable interrupt for OCR1A match
+    TCCR1B |= (1 << WGM12);                                    // CTC mode - reset timer on match
+    DDRD |= (1 << R_STEP_PIN) | (1 << L_STEP_PIN) | (1 << L_DIR_PIN) | (1 << R_DIR_PIN); // Set pins to output
 
-    #ifndef SERIAL 
-        Serial.begin(BAUD_RATE);
-    
-        byte ack = Serial.read(); // initialize the handshake variable
-        while(ack != ACK) {       // listen for the handchake character  
-            ack = Serial.read();
-        }
-        Serial.write(ACK); // acknowledge successful connection
-    #endif
 
+    Serial.begin(BAUD_RATE);
+
+    timer = millis();
 }
 
 void loop() {
-    // 1. Computer 
+    if(millis() - timer > 10) 
+    {
+        if(speed <= 500 && direction) 
+        {
+            speed++;
+            if(speed >= 500) {
+                direction = !direction;
+            }
+        }
+        if(speed >= 0 && !direction) 
+        {
+            speed--;
+            if(speed <= 0) {
+                direction = !direction;
+            }
+        }
+        if((int)speed % 2 == 0) {
+            skipRightPulse = 1;
+            skipLeftPulse = 0;
+        }
+        else if((int)speed % 100 == 0) {
+            skipLeftPulse = 1;
+            skipRightPulse = 0;
+        } 
+        else {
+            skipRightPulse = 0;
+            skipLeftPulse = 0;
+        }
+        set_pulse_duration(pulse_time(speed));
+        Serial.println(speed);
+        timer = millis();
+    }
 
-   }
+    if (direction)
+        {
+            PORTD &= ~(1 << L_DIR_PIN); // Normal direction
+            PORTD |= (1 << R_DIR_PIN);
+        }
+        else
+        {
+            PORTD |= (1 << L_DIR_PIN); // Reversed direction
+            PORTD &= ~(1 << R_DIR_PIN);
+        }
+
+
+}
+
+/*
+  Set duration for the pulse generated for stepper motors
+*/
+void set_pulse_duration(float pulseTime)
+{
+    if (pulseTime < 1)
+        return;
+    stop_timer();
+    OCR1A = MIN_TICKS * pulseTime;
+    TCNT1 = (TCNT1 < OCR1A) ? TCNT1 : 0; // Set the counter to 0, if OCR1A is smaller than TCNT1. - Risk of corrupting TCNT1, if timer is not disabled!
+    start_timer();
+}
+
+float pulse_time(float speed) 
+{
+    // Do this to avoid overflowing 16-bit register when going slow speed
+    // 1000 / 65535 = 0.01525...
+    // Keep the limit a bit higher because of Atmega328p's inaccurate floating point calculations
+    if (speed < 0.016f && speed > -0.016f) 
+    {
+        return MAX_SPEED / 0.016f;
+    }
+    return fabs(MAX_SPEED / speed);
+}
+
+/*
+  Sets prescaler to 0 to stop the timer
+*/
+inline void stop_timer()
+{
+    TCCR1B &= ~((1 << CS12) | (1 << CS11) | (1 << CS10));
+}
+/*
+  Sets prescaler to 64 to start the timer
+*/
+inline void start_timer()
+{
+    TCCR1B |= (1 << CS11) | (1 << CS10);
+}
+
+#define RMP_HIGH (PIND >> R_STEP_PIN) & 1
+#define LMP_HIGH (PIND >> L_STEP_PIN) & 1
+#define RMD_HIGH (PIND >> R_DIR_PIN) & 1
+
+ISR(TIMER1_COMPA_vect) 
+{
+    // Drive right motor 
+    if(skipRightPulse == 0) 
+    {
+        if(RMP_HIGH) {
+            // Set pin low
+            PORTD &= ~(1 << R_STEP_PIN);
+        }
+        else {
+            // Set pin high
+            PORTD |= (1 << R_STEP_PIN);
+        }
+    }
+    // Drive left motor
+    if(skipLeftPulse == 0) {
+        if(LMP_HIGH) {
+            // Set pin low
+            PORTD &= ~(1 << L_STEP_PIN);
+        }
+        else {
+            // Set pin high
+            PORTD |= (1 << L_STEP_PIN);
+        }
+    }
+    numInterrupts++;
+    if(numInterrupts % 2 == 0) {
+        actualPosition += (RMD_HIGH) ? -1 : 1;
+    }
 
 }
